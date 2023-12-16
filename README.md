fb8f68b717a60348f69793fafccdd5a4e3488e14
Zo-Snow <zcr.snow@gmail.com>
2023-12-16 14:11:23 +0300

    Write algorithm and complete step 1 - computer choice functionality

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors
+
+A basic rock paper scissors game which runs in the console to practise javascript assigned by the odin project.
+
+Adding GUI, css and styling later when assigned.
---
+++ index.js
@@ -0,0 +1,23 @@
+// Step 1 - Make a function which deals with computer selection of rock paper scissors
+
+const options = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+    // Get a random number to use as a random index to select an option from the options list
+
+    let randomIndex = Math.floor(Math.random() * 3);
+    let randomChoice = options[randomIndex];
+    return randomChoice
+}
+
+// Step 2 - Make a function which takes in the user choice 
+
+// Step 3 - Make a function which converts words to title case to match the options list words
+
+// Step 4 - Make a function which takes in the computer and user choice to play a round of the game 
+
+// Step 5 - Make a function which is called game to play the round 5 times keeping score and revealing a winner
+
+
+
+
