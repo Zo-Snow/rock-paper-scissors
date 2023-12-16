89f6b3824a94039524b7518680ca63e64926acb8
Zo-Snow <zcr.snow@gmail.com>
2023-12-16 14:27:17 +0300

    Complete step 2 and 3 - user choice functionality

--- index.js
+++ index.js
@@ -12,11 +12,31 @@ function getComputerChoice() {
 
 // Step 2 - Make a function which takes in the user choice 
 
+function getPlayerChoice() {
+    let userInput = prompt("Choose one: Rock, Paper, Scissors.");
+    let userChoice = makeTitleCase(userInput);
+    return userChoice
+}
+
 // Step 3 - Make a function which converts words to title case to match the options list words
+
+function makeTitleCase(word) {
+    // Converting word to lower and trimming out the whitespaces
+    let toLower = word.toLowerCase().trim();
+    // Converting the first letter to UpperCase and joining it with the rest of the word
+    word = toLower.slice(0, 1).toUpperCase() + toLower.slice(1, toLower.length);
+    return word
+}
 
 // Step 4 - Make a function which takes in the computer and user choice to play a round of the game 
 
 // Step 5 - Make a function which is called game to play the round 5 times keeping score and revealing a winner
+
+
+
+
+
+
 
 
 
